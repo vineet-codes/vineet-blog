df0beefbabd13a3f53e1bfe362b9913276ce8853
Vineet Singh <vineet.singh@Vineets-MacBook-Pro.local>
2025-12-10 00:28:18 +0530

    add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       base: '/vineet-blog/',
+      assetsInclude: ['**/*.md'],
       server: {
         port: 3000,
         host: '0.0.0.0',
